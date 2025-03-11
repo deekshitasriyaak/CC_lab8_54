779ec0933335bb7b95e497e30e0d23174ff49f35
deekshitasriyaak <deekshitasriyaak@gmail.com>
2025-03-11 15:42:46 +0530

    changes1

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this and changed this.  \n";
+  cout << "I have successfully built and run this and changed this.Hopefully.  \n";
   return 0;
 }
